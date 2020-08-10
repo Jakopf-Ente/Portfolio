b2b725420f9654bd8f4f89362ba1e2ebf3c99a1b
Jakob Schäfer <Jakobi@Jakobs-MBP.local>
2020-08-10 18:08:56 +0200

    Vue + Router + Bootstrap (Website Setup) help

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+      <navbar/>
+      <router-view/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import navbar from "@/Navbar/nav.vue"
+
+export default {
+  name: 'Index',
+  components: {
+    navbar,
+  },
+}
+
+</script>
